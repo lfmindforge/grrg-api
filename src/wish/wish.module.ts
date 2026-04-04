815e55b873b61a577a53a89095f1f4ca2a0d65c3
GrasPanda <Lucasus822@gmail.com>
2026-04-04 19:28:29 +0200

    updateWish Module

--- src/wish/wish.module.ts
+++ src/wish/wish.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WishController } from './wish.controller';
 import { WishService } from './wish.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Wish } from './wish.entity';
+import { SupabaseStorageService } from './supabase-storage.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Wish])],
   controllers: [WishController],
-  providers: [WishService],
+  providers: [WishService, SupabaseStorageService],
 })
 export class WishModule {}
