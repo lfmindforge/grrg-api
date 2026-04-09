e5bcbe96a7f35694e0f1ad974715293bb03d7211
GrasPanda <Lucasus822@gmail.com>
2026-04-09 20:41:38 +0200

    add DonationService + tests

---
+++ src/donations/donation.service.ts
@@ -0,0 +1,51 @@
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Donation } from './donation.entity';
+import { Wish } from '../wish/wish.entity';
+import { DonationType, WishStatus } from '../wish/wish.types';
+import { DonationStatus } from './donation.types';
+import { CreateDonationDto } from './dto/create-donation.dto';
+
+@Injectable()
+export class DonationService {
+  constructor(
+    @InjectRepository(Donation)
+    private readonly donationRepo: Repository<Donation>,
+    @InjectRepository(Wish)
+    private readonly wishRepo: Repository<Wish>,
+  ) {}
+
+  async propose(donorId: string, dto: CreateDonationDto): Promise<Donation> {
+    const wish = await this.wishRepo.findOne({ where: { id: dto.wish_id } });
+    if (!wish) throw new NotFoundException('Souhait introuvable');
+    if (wish.status !== WishStatus.PENDING) {
+      throw new BadRequestException('Ce souhait ne peut plus recevoir de dons');
+    }
+    if (wish.user_id === donorId) {
+      throw new ForbiddenException(
+        'Impossible de faire un don sur son propre souhait',
+      );
+    }
+
+    const donation = this.donationRepo.create({
+      wish_id: dto.wish_id,
+      donor_id: donorId,
+      type: dto.type,
+      amount: dto.type === DonationType.FINANCIAL ? (dto.amount ?? null) : null,
+      nature_description:
+        dto.type !== DonationType.FINANCIAL
+          ? (dto.nature_description ?? null)
+          : null,
+      is_anonymous: dto.is_anonymous ?? false,
+      status: DonationStatus.PENDING,
+    });
+
+    return this.donationRepo.save(donation);
+  }
+}
