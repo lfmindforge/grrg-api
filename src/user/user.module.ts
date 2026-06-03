8b146311b04f2b5573d2d3de41eb27d4142a297a
GrasPanda <Lucasus822@gmail.com>
2026-06-03 15:30:38 +0200

    computed followers/following in GEt /users/:id

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Wish } from '../wish/wish.entity';
 import { Donation } from '../donation/donation.entity';
+import { Follow } from '../follow/follow.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { StorageModule } from '../common/storage/storage.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Wish, Donation]), StorageModule],
+  imports: [TypeOrmModule.forFeature([User, Wish, Donation, Follow]), StorageModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
