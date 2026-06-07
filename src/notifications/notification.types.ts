ddc1d7c25240196aa5b40cfd1b4f4fb0f3744e60
GrasPanda <Lucasus822@gmail.com>
2026-06-07 16:24:48 +0200

    types NotificationsType + DTO

---
+++ src/notifications/notification.types.ts
@@ -0,0 +1,13 @@
+export enum NotificationType {
+  DONATION_RECEIVED = 'donation_received',
+  EVALUATION_RECEIVED = 'evaluation_received',
+  COMMENT_RECEIVED = 'comment_received',
+  REACTION_RECEIVED = 'reaction_received',
+  GRADE_UP = 'grade_up',
+  BADGE_EARNED = 'badge_earned',
+  NEW_FOLLOWER = 'new_follower',
+}
+
+export function truncateTitle(title: string, max = 50): string {
+  return title.length > max ? `${title.slice(0, max)}…` : title;
+}
