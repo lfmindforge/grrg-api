9697d7e2b82b308f1a8805326754a02d5fc5e1b5
GrasPanda <Lucasus822@gmail.com>
2026-06-07 17:22:35 +0200

    reaction_received on new reaction

--- src/reaction/reaction.module.ts
+++ src/reaction/reaction.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reaction } from './reaction.entity';
 import { Wish } from '../wish/wish.entity';
+import { User } from '../user/user.entity';
 import { ReactionService } from './reaction.service';
 import { ReactionController } from './reaction.controller';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reaction, Wish])],
+  imports: [TypeOrmModule.forFeature([Reaction, Wish, User]), NotificationsModule],
   controllers: [ReactionController],
   providers: [ReactionService],
   exports: [TypeOrmModule],
