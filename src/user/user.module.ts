b27f197c93af883ac8c03e8dd4a871f9e368073c
GrasPanda <Lucasus822@gmail.com>
2026-06-08 13:27:42 +0200

    Add refresh-tokens to UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,6 +4,7 @@ import { User } from './user.entity';
 import { Wish } from '../wish/wish.entity';
 import { Donation } from '../donation/donation.entity';
 import { Follow } from '../follow/follow.entity';
+import { RefreshToken } from '../auth/entities/refresh-token.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { StorageModule } from '../common/storage/storage.module';
@@ -11,7 +12,7 @@ import { FollowModule } from '../follow/follow.module';
 import { BadgeModule } from '../badge/badge.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Wish, Donation, Follow]), StorageModule, FollowModule, BadgeModule],
+  imports: [TypeOrmModule.forFeature([User, Wish, Donation, Follow, RefreshToken]), StorageModule, FollowModule, BadgeModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
