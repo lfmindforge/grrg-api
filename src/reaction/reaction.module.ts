5d190793a4f1c61df2ede1079b10d0a068525334
GrasPanda <Lucasus822@gmail.com>
2026-06-09 11:17:41 +0200

    log reaction.upsert, reaction.delete events

--- src/reaction/reaction.module.ts
+++ src/reaction/reaction.module.ts
@@ -6,9 +6,10 @@ import { User } from '../user/user.entity';
 import { ReactionService } from './reaction.service';
 import { ReactionController } from './reaction.controller';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { EventLogModule } from '../event-log/event-log.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reaction, Wish, User]), NotificationsModule],
+  imports: [TypeOrmModule.forFeature([Reaction, Wish, User]), NotificationsModule, EventLogModule],
   controllers: [ReactionController],
   providers: [ReactionService],
   exports: [TypeOrmModule],
