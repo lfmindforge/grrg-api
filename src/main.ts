6a621c95cf289c6083182fadfa17cbf1e41c29c8
GrasPanda <Lucasus822@gmail.com>
2026-06-10 16:20:01 +0200

    Install helmet, compression and graceful shutdown

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser = require('cookie-parser');
+import helmet from 'helmet';
+import compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // cookie-parser doit être enregistré avant les guards — il parse req.cookies utilisé dans AuthController
   app.use(cookieParser());
+  app.use(helmet());
+  app.use(compression());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
   });
+  app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
