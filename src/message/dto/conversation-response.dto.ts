c0a982f437eb4fb3ad9dd9ff65607f8d550f1995
GrasPanda <Lucasus822@gmail.com>
2026-06-13 14:02:11 +0200

    Add messages DTOs

---
+++ src/message/dto/conversation-response.dto.ts
@@ -0,0 +1,18 @@
+export class MessageResponseDto {
+  id!: string;
+  conversation_id!: string;
+  sender_id!: string;
+  sender_pseudo!: string;
+  sender_avatar!: string | null;
+  content!: string;
+  is_read!: boolean;
+  created_at!: Date;
+}
+
+export class ConversationResponseDto {
+  id!: string;
+  other_user!: { id: string; pseudo: string; avatar_url: string | null };
+  last_message!: MessageResponseDto | null;
+  unread_count!: number;
+  updated_at!: Date;
+}
