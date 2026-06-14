7cb3976cca73e145593e9d6565b41b2b418c7eb8
GrasPanda <Lucasus822@gmail.com>
2026-06-14 11:18:49 +0200

    Add Donation entity to forFeature TypeOrm in amdin Module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,13 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/user.entity';
 import { Wish } from '../wish/wish.entity';
 import { Comment } from '../comment/comment.entity';
+import { Donation } from '../donation/donation.entity';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { EventLogModule } from '../event-log/event-log.module';
 import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Wish, Comment]), EventLogModule, MailModule],
+  imports: [TypeOrmModule.forFeature([User, Wish, Comment, Donation]), EventLogModule, MailModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
